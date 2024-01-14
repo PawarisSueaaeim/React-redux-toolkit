46e97a3cd42133705864b7e22d22e7330b178f4a
PawarisSueaaeim <pawaris.sueaaeim@gmail.com>
2024-01-14 23:07:03 +0700

    start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 "use client"
-import Login from './login'
 import { useAppSelector } from '@/redux/store'
 
 export default function Home() {
@@ -8,8 +7,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Login/>
-      <h1>username: {username}</h1>
+      
     </main>
   )
 }
